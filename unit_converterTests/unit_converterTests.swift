b49655615bfdc6dbe76b0e114ab83d66523cf8fc
Shawn Mansfield <shawn.mansfield@gmail.com>
2020-05-19 06:56:20 -0400

    Initial Commit

---
+++ unit_converterTests/unit_converterTests.swift
@@ -0,0 +1,34 @@
+//
+//  unit_converterTests.swift
+//  unit_converterTests
+//
+//  Created by Shawn Mansfield on 5/19/20.
+//  Copyright © 2020 Shawn Mansfield. All rights reserved.
+//
+
+import XCTest
+@testable import unit_converter
+
+class unit_converterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
